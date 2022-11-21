ced33c6f1a4a3d040324f48502e6ced47378aa95
Daniel <d.j.sittlinger@student.tugraz.at>
2022-11-21 23:08:10 +0100

    Uploaded firmware for http-ota-update

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266httpUpdate.h>
 
+
+WiFiClient client;
 
 void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(4, OUTPUT);
   Serial.begin(115200);
   Serial.println("Started");
   WiFi.mode(WIFI_STA);
@@ -13,8 +18,23 @@ void setup() {
     ESP.restart();
   }
   Serial.println("WiFI Connected");
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(digitalRead(2)==LOW)
+  {
+    while(digitalRead(2)==LOW)
+    {
+      delay(50);
+    }
+    Serial.println("Update...");
+    ESPhttpUpdate.update(client, "https://github.com/Sitte6/H801-Lamp/firmware", 80, "/firmware.bin");
+  }
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+  delay(500);
+
 }
